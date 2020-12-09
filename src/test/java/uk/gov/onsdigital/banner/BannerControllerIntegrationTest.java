9bb39f96d5d7c35b0beb7bff5ceefbb04914719e
jterryons <john.terry@ons.gov.uk>
2020-12-09 13:33:56 +0000

    add datastore emulator for integration tests

---
+++ src/test/java/uk/gov/onsdigital/banner/BannerControllerIntegrationTest.java
@@ -0,0 +1,63 @@
+package uk.gov.onsdigital.banner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+import java.lang.invoke.MethodHandles;
+import java.util.List;
+import java.util.concurrent.TimeoutException;
+
+import com.google.cloud.datastore.testing.LocalDatastoreHelper;
+import com.google.datastore.v1.client.DatastoreHelper;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class BannerControllerIntegrationTest {
+
+  @BeforeAll
+  public static void setUpClass() throws Exception {
+    DatastoreEmulator.startDatastore();
+  }
+
+  @AfterAll
+  public static void cleanUpClass() throws Exception {
+    DatastoreEmulator.stopDatastore();
+  }
+
+  @BeforeEach
+  public void init() throws IOException {
+    DatastoreEmulator.clearDatastore();
+  }
+
+  @LocalServerPort
+	private int port;
+
+	@Autowired
+  private TestRestTemplate restTemplate;
+
+  @MockBean
+  private BannerRepository bannerRepo;
+  
+  @Test
+  public void willReturnBannersObject() {
+    Mockito.when(bannerRepo.findAll())
+      .thenReturn(List.of(BannerModel.builder().build()));
+    List<BannerModel> banners = this.restTemplate.getForObject("http://localhost:" + port + "/banner",
+        List.class);
+    
+    assertEquals(1, banners.size());
+  }
+}
