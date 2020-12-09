694109833bb7dfd96b764a3fe239c912c4c032d8
jterryons <john.terry@ons.gov.uk>
2020-12-09 12:27:44 +0000

    get all implementation

--- src/main/java/uk/gov/onsdigital/banner/BannerController.java
+++ src/main/java/uk/gov/onsdigital/banner/BannerController.java
@@ -1,10 +1,26 @@
 package uk.gov.onsdigital.banner;
 
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.commons.collections4.IteratorUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/banner")
 public class BannerController {
+
+  @Autowired
+  private BannerRepository bannerRepo;
   
+  @GetMapping("")
+  public ResponseEntity<List<BannerModel>> getBanners() {
+    Iterator<BannerModel> bannerIter = bannerRepo.findAll().iterator();
+    return ResponseEntity.ok()
+      .body(IteratorUtils.toList(bannerIter));
+  }
 }
---
+++ src/test/java/uk/gov/onsdigital/banner/BannerControllerUnitTest.java
@@ -0,0 +1,43 @@
+package uk.gov.onsdigital.banner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.apache.commons.collections4.IterableUtils;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@ExtendWith(MockitoExtension.class)
+public class BannerControllerUnitTest {
+  
+  @InjectMocks
+  private BannerController bannerController;
+
+  @Mock
+  private BannerRepository bannerRepo;
+
+  @Test
+  public void willReturnBanners() {
+    BannerModel expected1 = BannerModel.builder().title("test1").build();
+    BannerModel expected2 = BannerModel.builder().title("test2").build();
+
+    Mockito.when(bannerRepo.findAll())
+      .thenReturn(List.of(expected1, expected2));
+    ResponseEntity<List<BannerModel>> resp = bannerController.getBanners();
+
+    assertEquals(HttpStatus.OK, resp.getStatusCode());
+    
+    List<BannerModel> actualBanners = resp.getBody();
+
+    assertEquals(2, actualBanners.size());
+    assertEquals(expected1, actualBanners.get(0));
+    assertEquals(expected2, actualBanners.get(1));
+  }
+}
