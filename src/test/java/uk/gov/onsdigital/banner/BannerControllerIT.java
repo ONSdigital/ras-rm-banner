1529960734ce41364424be79f719786e03913077
jterryons <john.terry@ons.gov.uk>
2021-01-06 14:21:18 +0000

    add global controller advice for catching runtime exceptions

--- src/test/java/uk/gov/onsdigital/banner/BannerControllerIT.java
+++ src/test/java/uk/gov/onsdigital/banner/BannerControllerIT.java
@@ -7,6 +7,8 @@ import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
+import javax.naming.OperationNotSupportedException;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
---
+++ src/test/java/uk/gov/onsdigital/banner/ControllerAdviceIT.java
@@ -0,0 +1,39 @@
+package uk.gov.onsdigital.banner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@WebMvcTest(value = BannerController.class)
+public class ControllerAdviceIT {
+  
+  @Autowired
+  private MockMvc mockMvc;
+
+  @MockBean
+  private BannerService bannerService;
+  
+  @Test
+  public void willThrow500OnRuntimeException() throws Exception {
+    RequestBuilder requestBuilder = MockMvcRequestBuilders
+      .patch("/banner/1/active")
+      .accept(MediaType.APPLICATION_JSON);
+
+    Mockito.when(bannerService.setActiveBanner("1"))
+      .thenThrow(new IllegalStateException());
+    
+    MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+    
+    assertEquals(HttpStatus.INTERNAL_SERVER_ERROR.value(), result.getResponse().getStatus());
+  }
+}
