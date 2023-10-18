36b85de0b347dbdf647a8b00dc2b0c81af10abdf
Matt <matt.crocker@gmail.com>
2023-10-18 14:14:07 +0100

    Try adding package scan

--- src/main/java/uk/gov/onsdigital/banner/Application.java
+++ src/main/java/uk/gov/onsdigital/banner/Application.java
@@ -2,8 +2,10 @@ package uk.gov.onsdigital.banner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "uk.gov.onsdigital.banner.service" })
 public class Application {
 
   public static void main(String[] args) {
