73364580dcd228e93ba1c9d79d5bc9f77ebd7c8f
Matt <matt.crocker@gmail.com>
2023-10-18 14:06:12 +0100

    Remove annotations

--- src/main/java/uk/gov/onsdigital/banner/repository/BannerRepository.java
+++ src/main/java/uk/gov/onsdigital/banner/repository/BannerRepository.java
@@ -1,10 +1,7 @@
 package uk.gov.onsdigital.banner.repository;
 
-import org.springframework.stereotype.Repository;
-
 import com.google.cloud.spring.data.datastore.repository.DatastoreRepository;
 import uk.gov.onsdigital.banner.model.BannerModel;
 
-@Repository
 public interface BannerRepository extends DatastoreRepository<BannerModel, String> {
 }
--- src/main/java/uk/gov/onsdigital/banner/repository/TemplateRepository.java
+++ src/main/java/uk/gov/onsdigital/banner/repository/TemplateRepository.java
@@ -1,10 +1,7 @@
 package uk.gov.onsdigital.banner.repository;
 
-import org.springframework.stereotype.Repository;
-
 import com.google.cloud.spring.data.datastore.repository.DatastoreRepository;
 import uk.gov.onsdigital.banner.model.TemplateModel;
 
-@Repository
 public interface TemplateRepository extends DatastoreRepository<TemplateModel, Long> {
 }
